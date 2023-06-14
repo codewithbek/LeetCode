3d7cec8912c76e858b569aa5b4d92e6dd70d0e08
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2023-06-14 11:24:08 +0500

    the solution is optimized

--- bin/easy/problem018.dart
+++ bin/easy/problem018.dart
@@ -1,4 +1,4 @@
 // *557. Reverse Words in a String III
-String reverseWords(String s) =>
-    s.split('').reversed.join().split(' ').reversed.join(' ');
-
+// String reverseWords(String s) =>
+//     s.split('').reversed.join().split(' ').reversed.join(' ');
+String reverseWords(String s) => s.split(' ').map((word) => word.split('').reversed.join()).join(' ');
